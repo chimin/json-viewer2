43cf108f11a3d383ed11892b903d842aee41389d
chimin <fantazier@hotmail.com>
2020-11-29 00:37:15 +0800

    Fix expand state key is wrong

--- src/comps/ObjectRowViewer.tsx
+++ src/comps/ObjectRowViewer.tsx
@@ -17,8 +17,8 @@ export const ObjectRowViewer = ({
   path: string,
   level: number
 }) => {
-  const [isExpanded, setExpanded] = useLastStateBoolean(`${path}.isExpanded`, false);
-  const [valueViewerType, setValueViewerType] = useLastState<ValueViewerType>(`${path}.valueViewerType`, 'tree-view');
+  const [isExpanded, setExpanded] = useLastStateBoolean(`${path}/${label}.isExpanded`, false);
+  const [valueViewerType, setValueViewerType] = useLastState<ValueViewerType>(`${path}/${label}.valueViewerType`, 'tree-view');
   const valueIsSimpleType = isSimpleType(value);
   const valueIsTableType = !valueIsSimpleType && isTableType(value);
   const iconPaddingLeft = `${computeNestingOffset(level)}rem`;
