5496746e73401f6c667a07470eb63b89a44e6f9c
chimin <fantazier@hotmail.com>
2020-11-28 19:27:53 +0800

    Add link field

--- src/comps/ValueViewer.tsx
+++ src/comps/ValueViewer.tsx
@@ -2,8 +2,22 @@ import React from 'react';
 import { formatValue } from '../utils';
 import './Styles.css';
 
-export const ValueViewer = (props: { value: any }) => (
-  <span className={typeof props.value === 'string' ? 'value string' : 'value'}>
-    {formatValue(props.value)}
-  </span>
-);
+export const ValueViewer = (props: { value: any }) => {
+  if (typeof props.value === 'string') {
+    if (props.value.match(/https?:\/\//)) {
+      return (
+        <span className="value">
+          <a href={props.value} target="_blank">{props.value}</a>
+        </span>
+      );
+    }
+
+    return (
+      <span className="value string">{props.value}</span>
+    );
+  }
+
+  return (
+    <span className="value">{formatValue(props.value)}</span>
+  );
+};
